25c493d24e89200689d53c7ccb4a19f4236245fa
thomas.person <nateperson@yahoo.com>
2018-12-28 14:39:30 -0500

    python sort, looks and fast

--- run_clinvar_parse.py
+++ run_clinvar_parse.py
@@ -1,6 +1,7 @@
 """
 Alternate implementation of master.py.  Removing pysam, pypez, and non clinvar related functions.
-Making as pure python as can so can run on windows and *nix based systems.
+Making as python as can so can run on windows and *nix based systems.  Making complient for
+python3 as well as python2
 
 Run with -h to see all options.
 """
@@ -9,26 +10,32 @@ Run with -h to see all options.
 import os
 import sys
 from distutils import spawn
-
 import shutil
+import operator
+
 sys.path.insert(0, 'src'+os.sep)
 
 import parse_clinvar_xml as pcx
 
 try:
-    # For Python 3.0 and later
-    from urllib.request import urlopen
+	# For Python 3.0 and later
+	from urllib.request import urlopen
 except ImportError:
-    # Fall back to Python 2's urllib2
-    from urllib2 import urlopen
+	# Fall back to Python 2's urllib2
+	from urllib2 import urlopen
 
 try:
 	import configargparse
 	import pandas   # make sure all dependencies are installed
 except ImportError as e:
 	sys.exit("ERROR: Python module not installed. %s. Please run 'pip install -r requirements.txt' " % e)
-for executable in ['wget', 'tabix', 'vt', 'bgzip']:
+for executable in ['wget', 'tabix', 'vt', 'bgzip']:  #working on pure python implementation to remove need for these *nix based programs
 	assert spawn.find_executable(executable), "Command %s not found, see README" % executable
+
+
+def rreplace(s, old, new):
+	li = s.rsplit(old, 1) #Split only once
+	return new.join(li)
 
 
 def checkExists(fileAndPath):
@@ -38,16 +45,30 @@ def checkExists(fileAndPath):
 	else:
 		return False
 
+def sortRawTextFile(unSortedFile):
+	to_sort=open(unSortedFile,'r')
+	sorted_file=open(rreplace(unSortedFile,".",".sorted."),'wb')
+	header=0
+	file_lines=[]
+	for line in to_sort:
+		if header==0:
+			sorted_file.write(line)
+			header+=1
+		else:
+			file_lines.append([x.strip() for x in line.split('\t')])
+	to_sort.close()
+
+	for line in sorted(file_lines, key=operator.itemgetter(8)):
+		sorted_file.write("\t".join(line)+"\n")
+
+	sorted_file.close()
+	return rreplace(unSortedFile,".",".sorted.")
+
+
 def download_file(url,local_filename):
 	response = urlopen(url)
-
-	#r = requests.get(url, stream=True)
 	local_file=open(local_filename, 'wb')
 	local_file.write(response.read())
-
-	#for chunk in r.iter_content(chunk_size=1024):
-	#	if chunk:
-	#		f.write(chunk)
 	local_file.close()
 	return
 
@@ -70,11 +91,6 @@ def parseArguments():
 
 def main():
 
-	normalize_py="https://raw.githubusercontent.com/ericminikel/minimal_representation/master/normalize.py"
-	clinvar_xml="ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/xml/ClinVarFullRelease_00-latest.xml.gz"
-	clinvar_tsv="ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/tab_delimited/variant_summary.txt.gz"
-
-
 	cli_args=parseArguments()
 	print(cli_args)
 
@@ -94,7 +110,9 @@ def main():
 
 		os.makedirs(cli_args.output_tmp)
 
-
+	normalize_py="https://raw.githubusercontent.com/ericminikel/minimal_representation/master/normalize.py"
+	clinvar_xml="ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/xml/ClinVarFullRelease_00-latest.xml.gz"
+	clinvar_tsv="ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/tab_delimited/variant_summary.txt.gz"
 
 	if cli_args.download_new:
 		print("Downloading normalize.py")
@@ -134,7 +152,8 @@ def main():
 	if cli_args.b38fasta is not None:
 		if not checkExists(cli_args.output_tmp+"clinvar_table_raw.single.GRCh38.tsv") or cli_args.download_new:
 			pcx.parse_clinvar_tree(cli_args.xml_file, cli_args.output_tmp, 'GRCh38')   ##NOTE  ALSO, skipped sequences...  check other sequence locations?!?!  as each record can store multiple places!
-
+	sortRawTextFile(cli_args.output_tmp+"clinvar_table_raw.multi.GRCh38.tsv")
+	sortRawTextFile(cli_args.output_tmp+"clinvar_table_raw.single.GRCh38.tsv")
 	return
 
 
