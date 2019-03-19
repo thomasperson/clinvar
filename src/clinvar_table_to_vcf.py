ac1a8289ec0be7d2028f34af08243a247f22743d
thomasperson <tnperson@geisinger.edu>
2019-03-19 12:45:14 -0400

    singleLine

--- src/clinvar_table_to_vcf.py
+++ src/clinvar_table_to_vcf.py
@@ -5,9 +5,7 @@ import os
 import re
 import pandas as pd
 import sys
-
-#from parse_clinvar_xml import HEADER
-
+import time
 
 def gzopen(path, mode='r', verbose=True):
 	if path.endswith(".gz"):
@@ -104,6 +102,79 @@ def table_to_vcf(input_table_path, input_reference_genome, output_vcf):
 
 	outVCF.close()
 
+def table_to_vcf_oneline(input_table_path, input_reference_genome, output_vcf):
+	""""This method creates a vcf with a subset of available tsv fields, merged
+	into one vcf info field."""
+
+	DayMonthYear =time.strftime("%d")+time.strftime("%b")+time.strftime("%Y").upper()
+
+	# validate args
+	input_reference_genome_fai = input_reference_genome + ".fai"
+	if not os.path.isfile(input_table_path):
+		sys.exit("ERROR: %s not found" % input_table_path)
+	if not os.path.isfile(input_reference_genome_fai):
+		sys.exit("ERROR: %s (reference FASTA .fai) not found" %
+				 input_reference_genome_fai)
+
+	outVCF=open(output_vcf,'w')
+
+	# read input table. low_memory allows dtypes to be inferred
+	df = pd.read_table(gzopen(input_table_path), low_memory=False)
+
+	missing_columns = {"CHROM", "POS", "REF", "ALT"} - set(df.columns)
+	if missing_columns:
+		sys.exit("ERROR: %s is missing columns: %s" % (input_table_path, str(missing_columns)))
+
+	outVCF.write("##fileformat=VCFv4.3\n##source=clinvar\n")
+
+	df = df[['VARIATION_ID','ALLELE_ID','CHROM','POS','REF','ALT','NAME','TYPE','GENE_SYMBOL','CLINICAL_SIGNIFICANCE','CLIN_SIG_SIMPLE','LAST_EVALUATED','RS_NUM_DBSNP','REVIEW_STATUS','NUMBER_OF_SUBMITTERS','GOLD_STARS','CONFLICTED','CLIN_PATH']]
+
+	HEADER=list(df.columns.values)
+
+	loc_column = ['CHROM', 'POS', 'REF', 'ALT']
+
+	for l_c in loc_column:
+		HEADER.remove(l_c)
+
+	vcf_info_key="CLINVAR_"+DayMonthYear
+
+	outVCF.write("##INFO=<ID=CLINVAR_"+DayMonthYear+",Number=.,Type=String,Description=\"CLINVAR_" + DayMonthYear+": '"+"|".join(HEADER)+"'\">\n")
+
+	with open(input_reference_genome_fai) as in_fai:
+		for line in in_fai:
+			chrom, length, _ = line.split("\t", 2)
+			outVCF.write("##contig=<ID="+chrom.replace("chr", "")+",length="+length+">\n")
+	outVCF.write("##reference="+input_reference_genome+"\n")
+
+	outVCF.write("\t".join(["#CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO"])+"\n")
+	for i, table_row in df.iterrows():
+		vcf_row = []
+		vcf_row.append(table_row["CHROM"])
+		vcf_row.append(table_row["POS"])
+		vcf_row.append('.')  # ID
+		vcf_row.append(table_row["REF"])
+		vcf_row.append(table_row["ALT"])
+		vcf_row.append('.')  # QUAL
+		vcf_row.append('.')  # FILTER
+
+		info_field = collections.OrderedDict()
+
+
+
+		ClinVarOutInfoField=vcf_info_key+"="
+
+		for key in HEADER:
+			if pd.isnull(table_row[key]):
+				continue
+			value = str(table_row[key]).strip()
+			value = value.replace(":", "%3A").replace(";", "%3B").replace("=", "%3D").replace(" ", "_").replace(",", "%2C").strip()
+			ClinVarOutInfoField+=value+"|"
+
+		vcf_row.append(ClinVarOutInfoField.strip("|"))
+
+		outVCF.write("\t".join(map(str, vcf_row))+"\n")
+
+	outVCF.close()
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
